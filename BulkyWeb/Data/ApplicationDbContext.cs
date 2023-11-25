f3f817591441898a9c01a5706fa3eaaa2afea51b
Everest Nwosu <kvcchaudhary007@gmail.com>
2023-11-26 00:19:00 +0530

    new changes of CRDU anD DB connnections

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data
 {
@@ -7,6 +8,17 @@ namespace BulkyWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { id = 1, Name = "Action", DisplayOrder = 1 },
+              new Category { id = 2, Name = "SciFi", DisplayOrder = 2 },
+           new Category { id = 3, Name = "History", DisplayOrder = 3}
+                );
         }
     }
 }
