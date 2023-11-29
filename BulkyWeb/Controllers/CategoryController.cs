c83eb7a68b1adb6d4deeff442a4cfc62fcb33b2d
Everest Nwosu <kvcchaudhary007@gmail.com>
2023-11-29 17:14:54 +0530

    crud and tostr complete

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,20 +16,90 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList= _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Edit(int? id)
+        {
+            if(id==null && id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
         public IActionResult Create()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Category obj)
+        public IActionResult Create(Category obj )
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot excactly match the name.");
+            }
+            
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);    
+                _db.SaveChanges();
+                TempData["success"] = "Category created sucessfully";
+                    return RedirectToAction("Index");
+
+            }
+
+            
+            return View();
+            
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
         {
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["sucess"] = "Category edit succesfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null && id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int ? id)
+        {
+            Category obj = _db.Categories.Find(id);
+            
+                
+                if (obj == null)
+                {
+                    return NotFound();
+                }
+
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            TempData["sucess"] = "Category Deleted succesfully";
+            return RedirectToAction("Index");
+            
+            
         }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -13,7 +13,7 @@ namespace BulkyWeb.Models
         [MaxLength(30)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
-        [Range(1,100)]
+        [Range(1,100,ErrorMessage ="Display order must be 1 to 100")]
         public int DisplayOrder { get; set; }
     }
 }
